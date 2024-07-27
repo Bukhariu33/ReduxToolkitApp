4f00363fe3f1778ca77726546a21cb525d507527
Muhammad Uzair <101917043+Bukhariu33@users.noreply.github.com>
2024-07-27 18:49:13 +0500

    added second item in components folder

---
+++ src/components/secondItems.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SecondItems() {
+  return (
+    <View>
+      <Text>SecondItems</Text>
+    </View>
+  )
+}
